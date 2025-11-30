f591c0a0b8b24ca48434042c7796a2565af7668f
naeger <naeger.robert@gmail.com>
2025-11-30 16:50:50 +0100

    enable CORS from different methods (preflight)

--- src/main/kotlin/com/example/CORS.kt
+++ src/main/kotlin/com/example/CORS.kt
@@ -8,8 +8,13 @@ fun Application.configureCORS() {
     install(io.ktor.server.plugins.cors.routing.CORS) {
         allowMethod(HttpMethod.Post)
         allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Patch)
+        allowMethod(HttpMethod.Delete)
         allowHeader(HttpHeaders.ContentType)
+        allowHeader(HttpHeaders.Authorization)
         allowCredentials = true
-        allowHost("localhost:5173") // your frontend origin
+        allowHost("localhost:5173", schemes = listOf("http", "https"))
     }
 }
