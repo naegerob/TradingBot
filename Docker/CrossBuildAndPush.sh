1821342960cad3ebc23a6d254c7a8b91361e4552
Robert Näger, T-CH <robert.naeger@tecan.com>
2025-12-22 10:49:07 +0100

    change to --push because buildx does not store image locally

--- Docker/CrossBuildAndPush.sh
+++ Docker/CrossBuildAndPush.sh
@@ -4,7 +4,8 @@ docker buildx build \
   --platform linux/amd64,linux/arm64 \
   -t naegerr/tradingbot:latest \
   -f Docker/deployment/Dockerfile \
-  .
+  . \
+  --push
 
-docker tag naegerr/tradingbot:latest naegerr/tradingbot:build-9999
-docker push naegerr/tradingbot:build-9999
+#docker tag naegerr/tradingbot:latest naegerr/tradingbot:build-9999
+#docker push naegerr/tradingbot:build-9999
